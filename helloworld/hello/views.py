e1ad171bee3b2642f42b3ef8ebd8e835a5cf6362
Alison Major <alison.major@gmail.com>
2021-11-26 10:59:37 -0600

    Create view for Hello to specific name

--- helloworld/hello/views.py
+++ helloworld/hello/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 
+from .models import Name
+
 
 def index(request):
     return HttpResponse("Hello world!")
+
+
+def person(request, name_id):
+    return HttpResponse("Hello %s!" % Name.objects.get(id=name_id))
